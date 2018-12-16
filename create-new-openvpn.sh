bd33de0c9f768be38c0b9ef9a6bc17325ee3326c
Tony Yang <tianyuyang658@yahoo.com>
2018-12-16 15:08:12 -0800

    Update the SERVER IP command to something more reliable as dig may not always return the IP of a new cloud node

--- create-new-openvpn.sh
+++ create-new-openvpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SERVER_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+SERVER_IP=$(curl https://ipecho.net/plain)
 
 sudo bash -c "export SERVER_NAME='$SERVER_IP' && export CLIENT_NAME='clientkey' && export CLIENT_EMAIL='client@email.com' && docker-compose build openvpn && docker-compose up -d"
